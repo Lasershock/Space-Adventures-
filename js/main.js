45bd49285ed13a1be2264d64d5f17bace76eba84
Lasershock <michael.ladyzhensky777@gmail.com>
2023-07-21 12:15:46 -0400

    started second scene

--- js/main.js
+++ js/main.js
@@ -8,68 +8,33 @@ window.addEventListener('load',function(){
     canvas.width =500;
     canvas.height =500;
 
+    function welcome() {
     const space = document.getElementById('space');
     ctx.drawImage(space, 0, 0, canvas.width*1, canvas.height*1)
 
+    const moon = document.getElementById('moon');
+    ctx.drawImage(moon, 0, 200, 200, 200)
+
+    const earth = document.getElementById('earth');
+    ctx.drawImage(earth, 0, 0, 500, 500)
     
+    const npc = document.getElementById('npc');
+    ctx.drawImage(npc, 180, 100, 370, 400)
 
+    const bubble = document.getElementById('text-bubble');
+    ctx.drawImage(bubble,-10, -30, 400, 300) }
+    welcome()
 
+    function second_screen (){
+    const space = document.getElementById('space');
+    ctx.drawImage(space, 0, 0, canvas.width*1, canvas.height*1)
 
+    const npc = document.getElementById('npc');
+    ctx.drawImage(npc, 180, 100, 370, 400)
 
-
-
-
-
-
-
-    /*  const background_img = new Image(); // Create new img element
-background_img.src = "imgs/space-background.jpg";
-background_img.onload = function() {
-          ctx.drawImage(background_img, 0, 0, canvas.width*1, canvas.height*1);
-        };
-
-    const npc_img = new Image();
-npc_img.src = "imgs/npc.png";
-npc_img.onload = function(){
-        ctx.drawImage(npc_img, 170,110,380,400);
-};
-
-    const earth_img = new Image();
-earth_img.src = "imgs/earth.png";
-earth_img.onload = function(){
-        ctx.drawImage(earth_img, 0,30,730,500);
-};
-
-    const moon_img = new Image();
-moon_img.src = "imgs/moon.png";
-moon_img.onload = function(){
-        ctx.drawImage(moon_img, 20,260,170,170);
-};
-
-    const text_img = new Image();
-text_img.src = "imgs/text.png";
-text_img.onload = function(){
-        ctx.drawImage(text_img, 0,30,430,300);
-};
- //   };
-
-       //canvas.width*0.2
-
-    /* class Game {
-        constructor(width, height){
-            this.width =width;
-            this.height=height;
-            this.background = new background(this);
-        }
-        update(){
-            this.background.update();
-        }
-        draw(context){
-            this.background.draw(context);
-        }
+    const iss = document. getElementById('iss');
+    ctx.drawImage(iss, 300, 300, 0, 0)
     }
-
-    const game = new Game(canvas.width, canvas.height);
-    console.log(game); */
-
+    second_screen()
 });
+

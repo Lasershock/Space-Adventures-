3e91bed1a6a3e6f2ade6c91285758caaea0a551a
Lasershock <michael.ladyzhensky777@gmail.com>
2023-07-26 20:58:53 -0400

    screen 1 and 2 done (with text and transition)

--- js/main.js
+++ js/main.js
@@ -23,6 +23,12 @@ window.addEventListener('load',function(){
 
     const bubble = document.getElementById('text-bubble');
     ctx.drawImage(bubble,-10, -30, 400, 300) }
+
+    ctx.font = "15px Comic Sans MS";
+    ctx.fillStyle = "red";
+    ctx.textAlign = "center";
+    ctx.fillText("Hello World", 0, 0);
+
     welcome()
 
 
@@ -39,10 +45,15 @@ window.addEventListener('load',function(){
     const rev_bubble = document.getElementById('rev-text-bubble');
     ctx.drawImage(rev_bubble,-40, 270, 400, 300) 
 
+    ctx.font = "15px Comic Sans MS";
+    ctx.fillStyle = "black";
+    ctx.textAlign = "center";
+    ctx.fillText("This is the largest spaces station in the", 160, 390);
+    ctx.fillText("world...well outside of it, technically.", 148, 415);
+    ctx.fillText("Here,let me give you a tour", 115,440);
   
-
     }
     
-    second_screen()
+   // second_screen()
 });
 

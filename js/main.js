09177b4e2eaa45568b7bf5f5c25940973f51b732
Lasershock <michael.ladyzhensky777@gmail.com>
2023-07-28 18:55:40 -0400

    implemented delay function and screen 3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-/*import {background} from './background.js';
-import {earth} from './earth.js';
-import {npc} from './npc.js'; */
+var delayInMilliseconds = 1; //1000 = 1 second (final game will have 3000 (3 sec))
+
 
 
 
@@ -30,15 +29,20 @@ window.addEventListener('load',function(){
         ctx.font = "15px Comic Sans MS";
         ctx.fillStyle = "black";
         ctx.textAlign = "center";
-        ctx.fillText("Welcome, new recruite. I am glad that", 180, 43);
+        ctx.fillText("Welcome, new recruit. I am glad that", 180, 43);
         ctx.fillText("you could make it here safely. And by", 177, 73);
         ctx.fillText("here... I mean the ISS space station,", 175, 100);
-        ctx.fillText("of course.", 80, 130);
+        ctx.fillText("of course.", 80, 130); 
 
-    
-        canvas.addEventListener("click", (e) => {  
-            second_screen()  
-        }, { once: true });  
+        setTimeout(function(){
+
+            canvas.addEventListener("click", (e) => {  
+                second_screen()  
+            }, { once: true });  
+
+        },delayInMilliseconds)
+
+
       }
 
 
@@ -55,23 +59,113 @@ window.addEventListener('load',function(){
         const rev_bubble = document.getElementById('rev-text-bubble');
         ctx.drawImage(rev_bubble,-40, 270, 400, 300) 
 
+        const moon = document.getElementById('moon');
+        ctx.drawImage(moon, 350, -50, 200, 200)
+
         ctx.font = "15px Comic Sans MS";
         ctx.fillStyle = "black";
         ctx.textAlign = "center";
         ctx.fillText("The ISS is the largest spaces station in ", 160, 390);
         ctx.fillText("the world...well outside of it, technically.", 160, 415);
-        ctx.fillText("Here, let me give you a tour", 115,440);
+        ctx.fillText("Here, let me give you a tour-", 115,440);
 
+        
+        setTimeout(function(){
+
+            canvas.addEventListener("click", (e) => {  
+                third_screen()  
+            }, { once: true });  
+
+        },delayInMilliseconds)
+
+        
         const second_screen_abortController = new AbortController();
         document.addEventListener("keypress", (event) => {
         if (event.key=="Enter"){
+
+            {document.getElementById("Choice_1").style.display="none";}
+            {document.getElementById("Choice_2").style.display="none";}
+            {document.getElementById("Choice_3").style.display="none";}
+            {document.getElementById("Choice_4").style.display="none";}
             second_screen_abortController.abort()  
-            welcome()  
+            welcome()
         }
         }, {signal: second_screen_abortController.signal});
-  
+
+
+      }
+    
+
+   
+   
+    function third_screen (){
+        const space = document.getElementById('space');
+        ctx.drawImage(space, 0, 0, canvas.width*1, canvas.height*1)
+        
+        var clickMeButton = document.createElement('button');
+        clickMeButton.id = 'Choice_1';
+        clickMeButton.innerHTML = 'Choice 1';
+        clickMeButton.style.background = '#4FFF8F';
+        clickMeButton.style.width = '150px'; // Set the width to 100 pixels
+        clickMeButton.style.height = '50px'; // Set the height to 50 pixels
+        clickMeButton.style.position = 'absolute'; // To position the button with top and left
+        clickMeButton.style.top = '400px';
+        clickMeButton.style.left = '500px';
+        document.body.appendChild(clickMeButton);
+
+        var clickMeButton = document.createElement('button');
+        clickMeButton.id = 'Choice_2';
+        clickMeButton.innerHTML = 'Choice 2';
+        clickMeButton.style.background = '#4FFF8F';
+        clickMeButton.style.width = '150px'; // Set the width to 100 pixels
+        clickMeButton.style.height = '50px'; // Set the height to 50 pixels
+        clickMeButton.style.position = 'absolute'; // To position the button with top and left
+        clickMeButton.style.top = '400px';
+        clickMeButton.style.left = '800px';
+        document.body.appendChild(clickMeButton);
+
+        var clickMeButton = document.createElement('button');
+        clickMeButton.id = 'Choice_3';
+        clickMeButton.innerHTML = 'Choice 3';
+        clickMeButton.style.background = '#4FFF8F';
+        clickMeButton.style.width = '150px'; // Set the width to 100 pixels
+        clickMeButton.style.height = '50px'; // Set the height to 50 pixels
+        clickMeButton.style.position = 'absolute'; // To position the button with top and left
+        clickMeButton.style.top = '500px';
+        clickMeButton.style.left = '500px';
+        document.body.appendChild(clickMeButton);
+
+        var clickMeButton = document.createElement('button');
+        clickMeButton.id = 'Choice_4';
+        clickMeButton.innerHTML = 'Choice 4';
+        clickMeButton.style.background = '#4FFF8F';
+        clickMeButton.style.width = '150px'; // Set the width to 100 pixels
+        clickMeButton.style.height = '50px'; // Set the height to 50 pixels
+        clickMeButton.style.position = 'absolute'; // To position the button with top and left
+        clickMeButton.style.top = '500px';
+        clickMeButton.style.left = '800px';
+        document.body.appendChild(clickMeButton);
+
+        const npc = document.getElementById('npc');
+        ctx.drawImage(npc, 40, 140, 360, 390)
+
+        const moon = document.getElementById('moon');
+        ctx.drawImage(moon, -70, -50, 200, 200)
+
+        const bubble = document.getElementById('text-bubble');
+        ctx.drawImage(bubble,40, 10, 400, 300)
+        
+        ctx.font = "25px Comic Sans MS";
+        ctx.fillStyle = "black";
+        ctx.textAlign = "center";
+        ctx.fillText("PICK   YOUR   JOURNEY ", 245, 100);
+        ctx.font = "20px Comic Sans MS";
+        ctx.fillText("Where would you like to go?", 235, 170);
+
     }
 
     welcome()
 });
+
+
 
